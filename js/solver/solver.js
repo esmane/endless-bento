78772e201db4fcf32c1a7ca4914ddd730d438537
esmane <48299327+esmane@users.noreply.github.com>
2023-04-12 23:32:18 -0400

    Add files via upload

--- js/solver/solver.js
+++ js/solver/solver.js
@@ -33,3 +33,77 @@ function initSolver()
         }
     }
 }
+
+
+// this tells us where a clue can fit into the grid
+function solverCheckClueLocations(clueNo)
+{
+    // the first thing we need to do with a clue is see where it "fits" into the grid.
+    // what we do next depends on how many places it can fit and what sorts of tiles it contains.
+    // we will create a 1D array that tracks all the possible spots as strings of format x,y
+    var possibleLocations = [];
+    
+    // now let's check every spot on the grid the clue could physically fit
+    for(let gridX = 0; gridX <= (GRID_SIZE_W - globalClues[clueNo].length); gridX++)
+    {
+        for(let gridY = 0; gridY <= (GRID_SIZE_H - globalClues[clueNo][0].length); gridY++)
+        {
+            let fits = true;
+            // now we need to check every square of the clue
+            _clueCheckLoop:
+            for(let clueX = 0; clueX < globalClues[clueNo].length; clueX++)
+            {
+                _nextSquare:
+                for(let clueY = 0; clueY < globalClues[clueNo][0].length; clueY++)
+                {
+                    // what happens next depends on what the square of the clue is
+                    if(globalClues[clueNo][clueX][clueY] === "x-x")
+                    {
+                        break _nextSquare;
+                    }
+                    else if(globalClues[clueNo][clueX][clueY].charAt(0) === 'x')
+                    {
+                        // a clue that tells us shape only
+                        if(solverIsShapePossibility(gridX + clueX, gridY + clueY, globalClues[clueNo][clueX][clueY].charAt(2)))
+                        {
+                            break _nextSquare;
+                        }
+                        else
+                        {
+                            fits = false;
+                            break _clueCheckLoop;
+                        }
+                    }
+                    else if(globalClues[clueNo][clueX][clueY].charAt(2) === 'x')
+                    {
+                        // a clue that tells us shape only
+                        if(solverIsColorPossibility(gridX + clueX, gridY + clueY, globalClues[clueNo][clueX][clueY].charAt(0)))
+                        {
+                            break _nextSquare;
+                        }
+                        else
+                        {
+                            fits = false;
+                            break _clueCheckLoop;
+                        }
+                    }
+                    else if(solverIsPossibility(gridX + clueX, gridY + clueY, globalClues[clueNo][clueX][clueY])
+                    {
+                        break _nextSquare;
+                    }
+                    else
+                    {
+                        fits = false;
+                        break _clueCheckLoop;
+                    }
+                }
+            }
+            
+            if(fits)
+            {
+                possibleLocations.push(clueX + ',' + clueY);
+            }
+        }
+    }
+    return possibleLocations;   
+}
--- js/solver/solverutil.js
+++ js/solver/solverutil.js
@@ -11,7 +11,6 @@ function solverIsPossibility(x, y, tile)
     return false;
 }
 
-
 // these next two check if a given color or shape is a valid possibility for the given square. any tile with the given color or shape is a valid possibility
 function solverIsColorPossibility(x, y, color)
 {
@@ -36,6 +35,7 @@ function solverIsShapePossibility(x, y, shape)
     }
     return false;
 }
+
 
 
 // these next two tell you how many possibilities there are for the given square if it has to be a certain color or shape.
@@ -98,39 +98,79 @@ function solverGetShapePossibility(x, y, shape)
 }
 
 
-// these final three eliminate tiles from our possibilities
+
+// these next three eliminate tiles from our possibilities
 function solverEliminatePossibility(x, y, tile)
 {
     for(let i = solverPossibilityGrid[x][y].length - 1; i >= 0; i--)
     {
         if(solverPossibilityGrid[x][y][i] === tile)
         {
-            // we set it to an empty string rather than deleting the element. this is so our array stays a constant size.
-            // because our seeking functions look for the specific string this does not affect their operation in any way.
-            solverPossibilityGrid[x][y][i] = "";
+            solverPossibilityGrid[x][y].splice(i, 1);
             break;
         }
     }
 }
 
-function SolverEliminateColor(x, y, color)
+function solverEliminateColor(x, y, color)
 {
     for(let i = solverPossibilityGrid[x][y].length - 1; i >= 0; i--)
     {
         if(solverPossibilityGrid[x][y][i].charAt(0) === color)
         {
-            solverPossibilityGrid[x][y][i] = "";
+            solverPossibilityGrid[x][y].splice(i, 1);
         }
     }
 }
 
-function SolverEliminateShape(x, y, shape)
+function solverEliminateShape(x, y, shape)
 {
     for(let i = solverPossibilityGrid[x][y].length - 1; i >= 0; i--)
     {
         if(solverPossibilityGrid[x][y][i].charAt(2) === shape)
         {
-            solverPossibilityGrid[x][y][i] = "";
+            solverPossibilityGrid[x][y].splice(i, 1);
         }
+    }
+}
+
+
+
+// these next three functions are sort of the inverse of the eliminate functions.
+// they make the given tile the given square's only possibility,
+// and they eliminate it as an option from every other tile on the grid
+function solverSetSquare(x, y, tile)
+{
+    // first we want to eliminate the tile as a possibility from all squares
+    for(let i = 0; i < GRID_SIZE_W; i++)
+    {
+        for(let j = 0; j < GRID_SIZE_H; j++)
+        {
+            solverEliminatePossibility(i, j, tile);
+        }
+    }
+    
+    // then we want to make it the only possibility for the given tile
+    solverPossibilityGrid[x][y] = [tile];
+}
+
+function solverSetSquareColor(x, y, color)
+{
+    for(let i = 0; i < GRID_SIZE_W; i++)
+    {
+        if(i != color)
+        {
+            solverEliminateColor(x, y, i);
+        }
+    }
+}
+
+function solverSetSquareShape(x, y, shape)
+{
+    // then we want to make it the only possibility for the given tile
+    solverPossibilityGrid[x][y] = [];
+    for(let i = 0; i < GRID_SIZE_H; i++)
+    {
+        solverPossibilityGrid[x][y][i] = i + '-' + shape;
     }
 }
