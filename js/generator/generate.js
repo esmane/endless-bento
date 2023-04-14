1a479aaf0202d50d479d528bb59ae8602de68b92
esmane <48299327+esmane@users.noreply.github.com>
2023-04-14 12:38:22 -0400

    updated to create more sparse clues

--- js/generator/generate.js
+++ js/generator/generate.js
@@ -51,130 +51,57 @@ function generatePuzzle()
     }
     
     
-    // create the second clue
-    rand = Math.random();
-    switch(DIFFICULTY)
-    {
-        // easy mode always has a full size clue
-        case 0:
-            if(rand < 0.1)
-            {
-                rand = Math.random();
-                if(rand < 0.5)
-                {
-                    clueWidth--;
-                }
-                else
-                {
-                    clueHeight--;
-                }
-            }
-            else
-            {
-                clueWidth--;
-                clueHeight--;
-            }
-            break;
-            
-        // medium mode has them 40% of the time
-        case 1:
-            if(rand < 0.4)
-            {
-                rand = Math.random();
-                if(rand < 0.5)
-                {
-                    clueWidth--;
-                }
-                else
-                {
-                    clueHeight--;
-                }
-            }
-            else
-            {
-                clueWidth--;
-                clueHeight--;
-            }
-            break;
-            
-        // hard mode has them 5% of the time
-        case 2:
-            if(rand < 0.2)
-            {
-                rand = Math.random();
-                if(rand < 0.5)
-                {
-                    clueWidth--;
-                }
-                else
-                {
-                    clueHeight--;
-                }
-            }
-            else
-            {
-                clueWidth--;
-                clueHeight--;
-            }
-            break;
-    }
-    
-    generateClue(clueWidth, clueHeight);
-    
-    
     // the rest of the clues are generated in a loop
     // the random constants will be adjusted with testing to generate more fun puzzles
-    _l:
     while(!solverStep(0))
     {
-        rand = Math.random();
-        if(rand < 0.2)
+        // find a size for the new clue
+        clueWidth = Math.floor(Math.random() * (GRID_SIZE_W + 1));
+        clueHeight = Math.floor(Math.random() * (GRID_SIZE_H + 1));
+        
+        // we do not want to generate any 1x1 clues because they are useless, and we also do not want to generate any more full size clues because there should only be one of those per puzzle.
+        if(clueWidth === GRID_SIZE_W && clueHeight === GRID_SIZE_H)
         {
             rand = Math.random();
-            if(rand < 0.3)
+            if(rand < 0.5)
             {
-                rand = Math.random();
-                if(rand < 0.5)
-                {
-                    clueWidth--;
-                }
-                else
-                {
-                    clueHeight--;
-                }
+                clueWidth--;
             }
             else
             {
-                clueWidth--;
                 clueHeight--;
             }
-            
-            if(clueWidth * clueHeight < 2)
+        }
+        else if(clueWidth === 1 && clueHeight === 1)
+        {
+            rand = Math.random();
+            if(rand < 0.5)
             {
-                clueWidth = 2;
-                clueHeight = 2;
+                clueWidth++;
             }
-        }
-        
-        if(clueWidth > GRID_SIZE_W)
-        {
-            clueWidth = GRID_SIZE_W;
-        }
-        if(clueWidth > GRID_SIZE_H)
-        {
-            clueWidth = GRID_SIZE_H;
+            else
+            {
+                clueHeight++;
+            }
         }
         
         generateClue(clueWidth, clueHeight);
         
-        // safety
-        if(globalClues.length > 10)
+        // if we've generated more than 10 clues and the puzzle isn't solveable, chances are it is a bad puzzle. let's try again
+        if(globalClues.length >= 10)
         {
             generatePuzzle();
         }
     }
     
-    // once we escape this loop we have a solveable puzzle
+    // at this point we have a solveable puzzle generated. but how good is it?
+    // just as puzzles with more than 10 clues are bad puzzles, puzzles with less than 3 clues are also bad
+    if(globalClues.length < Math.max(GRID_SIZE_W, GRID_SIZE_H))
+    {
+        generatePuzzle();
+    }
+    
+    // now we have a good puzzle
     clearClues();
     for(let i = 0; i < globalClues.length; i++)
     {
@@ -231,17 +158,19 @@ function generateClue(w, h)
     switch(DIFFICULTY)
     {
         case 0:
-            maxSquares = Math.ceil(GRID_SIZE_W * GRID_SIZE_H / 2);
+            maxSquares = Math.ceil(w * h / 3);
             break;
             
         case 1:
-            maxSquares = Math.floor(GRID_SIZE_W * GRID_SIZE_H / 2);
+            maxSquares = Math.ceil(w * h / 4);
             break;
             
         case 2:
-            maxSquares = Math.ceil(GRID_SIZE_W * GRID_SIZE_H / 3);
+            maxSquares = Math.floor(w * h / 4);
             break;
     }
+    
+    maxSquares += 2;
     
     for(let clueX = 0; clueX < w; clueX++)
     {
@@ -255,7 +184,7 @@ function generateClue(w, h)
                 switch(DIFFICULTY)
                 {
                     case 0:
-                        if(rand < 0.5)
+                        if(rand < 0.7)
                         {
                             rand = Math.random();
                             if(rand < 0.5)
@@ -278,7 +207,7 @@ function generateClue(w, h)
                         
                     case 1:                        
                     case 2:
-                        if(rand < 0.35)
+                        if(rand < 0.8)
                         {
                             rand = Math.random();
                             if(rand < 0.5)
