94354d7acab6d162523ecb49c6a40f2eb6612385
esmane <48299327+esmane@users.noreply.github.com>
2023-04-14 00:27:37 -0400

    finished solver

--- js/solver/solver.js
+++ js/solver/solver.js
@@ -1,15 +1,11 @@
-var ALL_POSSIBILITIES;      // this stores all the possibilities that a square could be. useful for elimination
-
 var solverPossibilityGrid;  // 3d array of all the squares and all the tiles that each square could be
-var solverSolvedGrid;       // 2d array of the solved puzzle
 
 
 // this function prepares the solver global variables so that the solver can function properly
 function initSolver()
 {
-    ALL_POSSIBILITIES = initArray([GRID_SIZE_W * GRID_SIZE_H]);
+    var ALL_POSSIBILITIES = initArray([GRID_SIZE_W * GRID_SIZE_H]);
     solverPossibilityGrid = initArray([GRID_SIZE_W, GRID_SIZE_H, GRID_SIZE_W * GRID_SIZE_H]);
-    solverSolvedGrid = initArray([GRID_SIZE_W, GRID_SIZE_H]);
 
     let index = 0;
     for(let i = 0; i < GRID_SIZE_W; i++)
@@ -25,7 +21,6 @@ function initSolver()
     {
         for(let j = 0; j < GRID_SIZE_H; j++)
         {
-            solverSolvedGrid[i][j] = "x-x";
             for(let k = 0; k < GRID_SIZE_W * GRID_SIZE_H; k++)
             {
                 solverPossibilityGrid[i][j][k] = ALL_POSSIBILITIES[k];
@@ -35,18 +30,96 @@ function initSolver()
 }
 
 
-// the main solver function
+
+// this is the main solver function. it steps through the grid and all clues and eliminates as necessary.
+// this function will return true if the puzzle has been solved and false if it hasn't
+// this function is how the generator links with the solver.
+// the generator will invoke the solver by calling this function every time it creates a few new clues.
+// this is in hopes that the puzzle is now solveable.
+// if it gets a return true, the generator knows it has generated a solveable puzzle.
+// if it gets a return false, the generator knows it needs to make some more clues.
+// the startClue argument tells the solver what clue it should start by parsing, to make it a little more efficient
+function solverStep(startClue)
+{
+    // begin first pass of clues, starting with the clue specified
+    for(let i = startClue; i < globalClues.length; i++)
+    {
+        solverEliminateClues(i);
+    }
+    
+    // after checking all the clues is the perfect time to eliminate logically
+    solverEliminateLogically();
+    
+    // now let's check if the puzzle has been solved
+    var solved = true;
+    _solveCheckLoop:
+    for(let gridX = 0; gridX < GRID_SIZE_W; gridX++)
+    {
+        for(let gridY = 0; gridY < GRID_SIZE_H; gridY++)
+        {
+            if(solverPossibilityGrid[gridX][gridY].length != 1)
+            {
+                solved = false;
+                break _solveCheckLoop;
+            }
+        }
+    }
+    
+    if(solved)
+    {
+        return true;
+    }
+    
+    // if the puzzle hasn't been solved yet, begin pass two
+    // begin first pass of clues, starting with the clue specified
+    for(let i = startClue; i < globalClues.length; i++)
+    {
+        solverEliminateClues(i);
+    }
+    
+    // after checking all the clues is the perfect time to eliminate logically
+    solverEliminateLogically();
+    
+    // now let's check if the puzzle has been solved
+    solved = true;
+    _solveCheckLoop2:
+    for(let gridX = 0; gridX < GRID_SIZE_W; gridX++)
+    {
+        for(let gridY = 0; gridY < GRID_SIZE_H; gridY++)
+        {
+            if(solverPossibilityGrid[gridX][gridY].length != 1)
+            {
+                solved = false;
+                break _solveCheckLoop2;
+            }
+        }
+    }
+    
+    if(solved)
+    {
+        return true;
+    }
+    
+    return false;
+}
+
+
+
+// this function is probably the most important solver function.
+// it eliminates possiblities based on a single clue, using both absolute and relative elimination
 function solverEliminateClues(clueNo)
 {
     var possibleLocations = solverCheckClueLocations(clueNo);
+    var gridX = 0;
+    var gridY = 0;
     
     // if there is only one possible spot, absolute eliminate the clue
     if(possibleLocations.length === 1)
     {
         // x and y refer to the clue's location in the grid
         // because this is absolute elimination, there is only one possible location for the clue.
-        var gridX = Number(possibleLocations[0].charAt(0));
-        var gridY = Number(possibleLocations[0].charAt(2));
+        gridX = Number(possibleLocations[0].charAt(0));
+        gridY = Number(possibleLocations[0].charAt(2));
         
         // this repeats
         for(let clueX = 0; clueX < globalClues[clueNo].length; clueX++)
@@ -79,9 +152,55 @@ function solverEliminateClues(clueNo)
     }
     else
     {
-        // relative elimination           
+        // relative elimination
+        // unlike with absolute elimination, we can only do relative elimination some of the time
+        // we can only do relative elimination on clues that tell us both the color and shape of certain squares
+        // we can also do some weird things if we have a clue that contains all tiles of a certain color or shape
+        // for example in a 3x3 puzzle a clue with three 0-x squares
+        // however these require some special programming to detect because we need to search for them and so we are going to ignore this method of solving for now
+        // let's begin. first, we need to see if the clue contains any square that tell us both
+        for(let clueX = 0; clueX < globalClues[clueNo].length; clueX++)
+        {
+            for(let clueY = 0; clueY < globalClues[clueNo][0].length; clueY++)
+            {
+                if(globalClues[clueNo][clueX][clueY].charAt(0) != 'x' && globalClues[clueNo][clueX][clueY].charAt(2) != 'x')
+                {
+                    // if a tile does in fact tell us both, we need to eliminate that tile from every single squares possibilities
+                    for(let gridX = 0; gridX < GRID_SIZE_W; gridX++)
+                    {
+                        for(let gridY = 0; gridY < GRID_SIZE_H; gridY++)
+                        {
+                            solverEliminatePossibility(gridX, gridY, globalClues[clueNo][clueX][clueY]);
+                        }
+                    }
+                }
+            }
+        }
+        
+        // now that we've eliminated the possiblities, we need to add them back but only to the spots where they could fit according to the clue
+        // this means we need to try for every previously determined spot that the clue could fit
+        for(let i = 0; i < possibleLocations.length; i++)
+        {
+            gridX = Number(possibleLocations[0].charAt(0));
+            gridY = Number(possibleLocations[0].charAt(2));
+            
+            // this repeats
+            for(let clueX = 0; clueX < globalClues[clueNo].length; clueX++)
+            {
+                for(let clueY = 0; clueY < globalClues[clueNo][0].length; clueY++)
+                {
+                    // if a square in the clue fits the pattern
+                    if(globalClues[clueNo][clueX][clueY].charAt(0) != 'x' && globalClues[clueNo][clueX][clueY].charAt(2) != 'x')
+                    {
+                        // add it back only for the square that matches that clue in that clue location
+                        solverPossibilityGrid[gridX + clueX][gridY + clueY].push(globalClues[clueNo][clueX][clueY]);
+                    }
+                }
+            }
+        }
     }
 }
+
 
 
 // this tells us where a clue can fit into the grid
@@ -155,4 +274,24 @@ function solverCheckClueLocations(clueNo)
         }
     }
     return possibleLocations;   
+}
+
+
+
+// this final function eliminates logically
+// the purpose of this function is to eliminate according to this rule:
+// if a square has only one possibility, that square must contain the possibility.
+// therefore, that possibility cannot fit anywhere else in the grid, so we can eliminate it as a possibility from all other squares
+function solverEliminateLogically()
+{
+    for(let gridX = 0; gridX < GRID_SIZE_W; gridX++)
+    {
+        for(let gridY = 0; gridY < GRID_SIZE_H; gridY++)
+        {
+            if(solverPossibilityGrid[gridX][gridY].length === 1)
+            {
+                solverSetSquare(gridX, gridY, solverPossibilityGrid[gridX][gridY][0]);
+            }
+        }
+    }
 }
--- js/solver/solverutil.js
+++ js/solver/solverutil.js
@@ -38,68 +38,7 @@ function solverIsShapePossibility(x, y, shape)
 
 
 
-// these next two tell you how many possibilities there are for the given square if it has to be a certain color or shape.
-function solverNumberOfColorPossibilities(x, y, color)
-{
-    var count = 0;
-    for(let i = 0; i < solverPossibilityGrid[x][y].length; i++)
-    {
-        if(solverPossibilityGrid[x][y][i].charAt(0) === color)
-        {
-            count++;
-        }
-    }
-    return count;
-}
-
-function solverNumberOfShapePossibilities(x, y, shape)
-{
-    var count = 0;
-    for(let i = 0; i < solverPossibilityGrid[x][y].length; i++)
-    {
-        if(solverPossibilityGrid[x][y][i].charAt(2) === shape)
-        {
-            count++;
-        }
-    }
-    return count;
-}
-
-
-// these next two tell you what tile the square has to be if it has to be a certain color or shape and there is only one possibility for that square for that color or shape
-function solverGetColorPossibility(x, y, color)
-{
-    if(solverNumberOfColorPossibilities(x, y, color) === 1)
-    {
-        for(let i = 0; i < solverPossibilityGrid[x][y].length; i++)
-        {
-            if(solverPossibilityGrid[x][y][i].charAt(0) === color)
-            {
-                return solverPossibilityGrid[x][y][i];
-            }
-        }
-    }
-    return undefined;
-}
-
-function solverGetShapePossibility(x, y, shape)
-{
-    if(solverNumberOfShapePossibilities(x, y, shape) === 1)
-    {
-        for(let i = 0; i < solverPossibilityGrid[x][y].length; i++)
-        {
-            if(solverPossibilityGrid[x][y][i].charAt(2) === shape)
-            {
-                return solverPossibilityGrid[x][y][i];
-            }
-        }
-    }
-    return undefined;
-}
-
-
-
-// these next three eliminate tiles from our possibilities
+// this next one eliminates tiles from our possibilities
 function solverEliminatePossibility(x, y, tile)
 {
     for(let i = solverPossibilityGrid[x][y].length - 1; i >= 0; i--)
@@ -108,28 +47,6 @@ function solverEliminatePossibility(x, y, tile)
         {
             solverPossibilityGrid[x][y].splice(i, 1);
             break;
-        }
-    }
-}
-
-function solverEliminateColor(x, y, color)
-{
-    for(let i = solverPossibilityGrid[x][y].length - 1; i >= 0; i--)
-    {
-        if(solverPossibilityGrid[x][y][i].charAt(0) === color)
-        {
-            solverPossibilityGrid[x][y].splice(i, 1);
-        }
-    }
-}
-
-function solverEliminateShape(x, y, shape)
-{
-    for(let i = solverPossibilityGrid[x][y].length - 1; i >= 0; i--)
-    {
-        if(solverPossibilityGrid[x][y][i].charAt(2) === shape)
-        {
-            solverPossibilityGrid[x][y].splice(i, 1);
         }
     }
 }
@@ -154,6 +71,8 @@ function solverSetSquare(x, y, tile)
     solverPossibilityGrid[x][y] = [tile];
 }
 
+
+// this one eliminates all the possibilities that are NOT the color specified
 function solverSetSquareColor(x, y, color)
 {
     for(let i = solverPossibilityGrid[x][y].length - 1; i >= 0; i--)
