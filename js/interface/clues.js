8a9278163bc1546e527e40d2e334b28be68de6a9
esmane <48299327+esmane@users.noreply.github.com>
2023-04-17 15:52:20 -0400

    shrink clues for puzzle sizes larger than 3x3

--- js/interface/clues.js
+++ js/interface/clues.js
@@ -1,16 +1,24 @@
 function drawClue(clue)
 {
+    var scale = 50 * (3 / Math.max(GRID_SIZE_W, GRID_SIZE_H));
     var toAppend = "<div class='clue-grid'>";
     for(let i = 0; i < clue.length; i++)
     {
-        toAppend += "<div class='clue-row'>"
+        toAppend += "<div class='clue-row'";
+        toAppend += " style='height: ";
+        toAppend += scale;
+        toAppend += "px;'>"
         for(let j = 0; j < clue[i].length; j++)
         {
             toAppend += "<div class='clue-item'><img class='tile-50' src='./tiles/";
-            
             toAppend += clue[i][j];           
-            
-            toAppend += ".png' alt='tile'></div>";
+            toAppend += ".png' alt='";
+            toAppend += clue[i][j];
+            toAppend += "' style='width: ";
+            toAppend += scale;
+            toAppend += "px; height: ";
+            toAppend += scale;
+            toAppend += "px;'></div>";
         }
         toAppend += "</div>";
     }
