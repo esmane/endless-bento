4fac351d3fa272a1f55be0bc55db8e276aa636ab
esmane <48299327+esmane@users.noreply.github.com>
2023-04-17 23:24:43 -0400

    added lots more menus

--- js/interface/menu.js
+++ js/interface/menu.js
@@ -1,26 +1,60 @@
-var menuIsOpen = false;
+var menuIsOpen = 'n';   // 'n' for play, 's' for settings, 'h' for help, 'a' for about
 
-function openMenu()
+function openMenu(newMenu)
 {
-    if(menuIsOpen)
+    // first close the old menu
+    switch(menuIsOpen)
     {
-        menuIsOpen = false;
-        document.getElementById("menu").style.display = "none";
-        document.getElementById("left-side").style.display = "inline-block";
-        document.getElementById("right-side").style.display = "inline-block";
-        document.getElementById("menu-button").innerHTML = "menu";
-        document.getElementById("main-header").innerHTML = "endless bento";
+        case 'n':
+            document.getElementById("left-side").style.display = "none";
+            document.getElementById("right-side").style.display = "none";
+            document.getElementById("play-button").className = "ui-button";
+            break;
+            
+        case 's':
+            document.getElementById("menu").style.display = "none";
+            document.getElementById("menu-button").className = "ui-button";
+            break;
+            
+        case 'h':
+            document.getElementById("help").style.display = "none";
+            document.getElementById("help-button").className = "ui-button";
+            break;
+            
+        case 'a':
+            document.getElementById("about").style.display = "none";
+            document.getElementById("about-button").className = "ui-button";
+            break;
     }
-    else
+    
+    // then open new menu
+    switch(newMenu)
     {
-        menuIsOpen = true;
-        document.getElementById("menu").style.display = "inline";
-        document.getElementById("left-side").style.display = "none";
-        document.getElementById("right-side").style.display = "none";
-        document.getElementById("menu-button").innerHTML = "back";
-        document.getElementById("main-header").innerHTML = "settings";
+        case 'n':
+            document.getElementById("left-side").style.display = "inline-block";
+            document.getElementById("right-side").style.display = "inline-block";
+            document.getElementById("play-button").className = "selected-ui-button";
+            break;
+            
+        case 's':
+            document.getElementById("menu").style.display = "inline-block";
+            document.getElementById("menu-button").className = "selected-ui-button";
+            break;
+            
+        case 'h':
+            document.getElementById("help").style.display = "inline-block";
+            document.getElementById("help-button").className = "selected-ui-button";
+            break;
+            
+        case 'a':
+            document.getElementById("about").style.display = "inline-block";
+            document.getElementById("about-button").className = "selected-ui-button";
+            break;
     }
+    
+    menuIsOpen = newMenu;
 }
+
 
 function setBackground()
 {
@@ -29,7 +63,10 @@ function setBackground()
         document.body.style.backgroundColor = "#253330";
         
         document.getElementById("header").style.backgroundColor = "#131918";
+        document.getElementById("play-button").style.color = "#ffffff";
         document.getElementById("menu-button").style.color = "#ffffff";
+        document.getElementById("help-button").style.color = "#ffffff";
+        document.getElementById("about-button").style.color = "#ffffff";
         document.getElementById("restart-button").style.color = "#ffffff";
         
         document.body.style.color = "#ffffff";
@@ -40,7 +77,10 @@ function setBackground()
         document.body.style.backgroundColor = "#baffee";
         
         document.getElementById("header").style.backgroundColor = "#a9eedd";
+        document.getElementById("play-button").style.color = "#000000";
         document.getElementById("menu-button").style.color = "#000000";
+        document.getElementById("help-button").style.color = "#000000";
+        document.getElementById("about-button").style.color = "#000000";
         document.getElementById("restart-button").style.color = "#000000";
         
         document.body.style.color = "#000000";
