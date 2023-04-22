7a77eab348b251689642152bc59c608d0153f883
esmane <48299327+esmane@users.noreply.github.com>
2023-04-22 00:01:23 -0400

    added functions for working with cookies

--- js/util.js
+++ js/util.js
@@ -1,3 +1,4 @@
+// first two "util" functions deal with arrays
 // this function initializes an empty array of any sizes of any dimension
 // it takes an unlimited number of parameters IN AN ARRAY and generates arrays with recursion
 // stolen from https://stackoverflow.com/questions/12588618/javascript-n-dimensional-array-creation
@@ -45,4 +46,38 @@ function equal(array1, array2)
         }
     }
     return true;
+}
+
+
+
+// these next two functions deal with cookies
+// source: https://www.w3schools.com/js/js_cookies.asp
+function setCookie(name, value, daysToExpire)
+{
+    const d = new Date();
+    d.setTime(d.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+
+function getCookie(name)
+{
+    name += "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    
+    for(let i = 0; i < ca.length; i++)
+    {
+        let c = ca[i];
+        while(c.charAt(0) == ' ')
+        {
+            c = c.substring(1);
+        }
+        if(c.indexOf(name) == 0)
+        {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
